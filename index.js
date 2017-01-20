2f351ebca8665f00aa9f0f3598f66af2dcaddfa5
michaelrhodes <spam@michaelrhod.es>
2017-01-19 16:42:45 -0800

    Fix slice bug

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ function clean (ls, i) {
 }
 
 function slice (ai, o) {
-  var a = [], l = ai.length
-  for (o = o || 0; o < l; o++) a[o] = ai[o]
+  var a = [], i = 0, l = ai.length
+  for (o = o || 0; i < l; i++)
+    a[i] = ai[i + o]
   return a
 }
